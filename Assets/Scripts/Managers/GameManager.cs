7e87537e9f345076f763823101b642c521bd0ce1
JackHanni <jhanni@gci.net>
2024-04-29 12:46:04 -0600

    Built webgl
    With levels, but no character in levels.

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -1,7 +1,41 @@
-namespace Managers
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
 {
-    public class GameManager
+    public static GameManager manager;
+    int sceneNumber;
+
+    private void Awake()
     {
-        
+        if (manager == null) {
+            manager = this;
+            DontDestroyOnLoad(this);
+        } else if(manager != this) {
+            Destroy(gameObject);
+        }
+    }
+
+    void Start()
+    {
+        sceneNumber = 0;
+    }
+
+    void Update() 
+    {
+        if (sceneNumber < 3 && Input.GetKeyDown(KeyCode.Space)) {
+            NextScene();
+        }
+    }
+
+    public void NextScene()
+    {
+        sceneNumber++;
+        if (sceneNumber > 3) {
+            sceneNumber = 0;
+        }
+        SceneManager.LoadScene(sceneNumber);
     }
 }
