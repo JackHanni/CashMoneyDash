04b448a9cea1cfd40176e2199e2b94b228e6846d
Ruimin Zhang <ruiminz1@gmail.com>
2024-05-07 15:27:11 -0700

    player and slime data

---
+++ Assets/Scripts/Combat/AttackData_SO.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName ="New Data", menuName ="Attack/Attack Data")]
+public class AttackData_SO : ScriptableObject
+{
+    
+}
