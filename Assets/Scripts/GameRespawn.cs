bd96284632fe778c8da5a2937c93120e9bdfd79b
Ruimin Zhang <ruiminz@dyn-10-108-68-205.wless.uoregon.edu>
2024-05-18 02:01:20 -0700

    respawn

---
+++ Assets/Scripts/GameRespawn.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameRespawn : MonoBehaviour
+{
+    public float threshold = -6;
+    private CharacterController characterController;
+
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    void FixedUpdate()
+    {
+        if (characterController.transform.position.y < threshold)
+        {
+            Vector3 resetPosition = new Vector3(0, 1, 0);
+            characterController.enabled = false;
+            characterController.transform.position = resetPosition;
+            characterController.enabled = true;
+        }
+    }
+}
+
+
+
---
+++ Assets/Scripts/RespawnManager.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+
+public class RespawnManager : MonoBehaviour
+{
+    public float threshold = -6f;
+    private CharacterController characterController;
+    public Transform[] respawnPoints; // Array of respawn points
+
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        if (characterController.transform.position.y < threshold)
+        {
+            Transform nearestRespawnPoint = GetNearestRespawnPoint();
+            if (nearestRespawnPoint != null)
+            {
+                characterController.enabled = false;
+                characterController.transform.position = nearestRespawnPoint.position;
+                characterController.enabled = true;
+            }
+        }
+    }
+
+    Transform GetNearestRespawnPoint()
+    {
+        Transform nearestPoint = null;
+        float minDistance = float.MaxValue;
+
+        foreach (Transform respawnPoint in respawnPoints)
+        {
+            float distance = Vector3.Distance(characterController.transform.position, respawnPoint.position);
+            if (distance < minDistance)
+            {
+                minDistance = distance;
+                nearestPoint = respawnPoint;
+            }
+        }
+
+        return nearestPoint;
+    }
+}
