aec160b595b28702722cfb9e3f06bd9c68351fd6
Ruimin Zhang <ruiminz@dyn-10-108-83-53.wless.uoregon.edu>
2024-05-22 23:27:50 -0700

    update coin and gem sound effects

--- Assets/Scripts/Collectibles/Coin.cs
+++ Assets/Scripts/Collectibles/Coin.cs
@@ -5,18 +5,24 @@ using UnityEngine;
 public class Coin : MonoBehaviour
 {
     public GameObject pickupEffect;
+    public AudioClip audioEffect;
+
 
     private void OnTriggerEnter(Collider other)
     {
         PlayerInventory playerInventory = other.GetComponent<PlayerInventory>();
         if (playerInventory != null)
         {
+            // increment coin count
             playerInventory.CoinCollected();
             gameObject.SetActive(false);
 
+            // play audio
+            AudioSource.PlayClipAtPoint(audioEffect, playerInventory.transform.position);
+
+            // show pick up effects
             Instantiate(pickupEffect, transform.position, transform.rotation);
             Destroy(gameObject);
-            
         }
 
 
--- Assets/Scripts/Collectibles/Gem.cs
+++ Assets/Scripts/Collectibles/Gem.cs
@@ -4,8 +4,23 @@ using UnityEngine;
 
 public class Gem : MonoBehaviour
 {
+    //public GameObject pickupEffect;
+    public AudioClip audioEffect;
+
+
     private void OnTriggerEnter(Collider other)
     {
-        gameObject.SetActive(false);
+        PlayerInventory playerInventory = other.GetComponent<PlayerInventory>();
+        if (playerInventory != null)
+        {
+            gameObject.SetActive(false);
+
+            // play audio
+            AudioSource.PlayClipAtPoint(audioEffect, playerInventory.transform.position);
+
+            // show pick up effects - win
+            //Instantiate(pickupEffect, transform.position, transform.rotation);
+            Destroy(gameObject);
+        }
     }
 }
